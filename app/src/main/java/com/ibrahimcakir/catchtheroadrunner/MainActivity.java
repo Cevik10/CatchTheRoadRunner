bcbe98f915fd2557ae0c92e33b375e451ae76e8e
Hakan Çevik <hakanxcevik7@gmail.com>
2022-04-22 20:13:00 +0300

    vol5 button editing

--- app/src/main/java/com/ibrahimcakir/catchtheroadrunner/MainActivity.java
+++ app/src/main/java/com/ibrahimcakir/catchtheroadrunner/MainActivity.java
@@ -1,40 +1,52 @@
 package com.ibrahimcakir.catchtheroadrunner;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.PopupMenu;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    Button button1;
-    Button button4;
-    Button button5;
+    Button start_button;
+    Button freeMod_button;
+    Button arcadeMod_button;
+    boolean isOpen;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        isOpen=true;
+
         // Locate the buttons in your xml
-        button1 = (Button) findViewById(R.id.button1);
-        button4 = (Button) findViewById(R.id.button4);
-        button5 = (Button) findViewById(R.id.button5);
+        start_button = (Button) findViewById(R.id.Start_button);
+        freeMod_button = (Button) findViewById(R.id.FreeMod_button);
+        arcadeMod_button = (Button) findViewById(R.id.ArcadeMod_button);
 
         //set listener on button1
-        button1.setOnClickListener(new View.OnClickListener() {
+        start_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //make button2 visible when button1 is pressed
-                button4.setVisibility(View.VISIBLE);
-                button5.setVisibility(View.VISIBLE);
+                if (isOpen) {
+                    arcadeMod_button.setVisibility(View.VISIBLE);
+                    freeMod_button.setVisibility(View.VISIBLE);
+                    isOpen=false;
+                }
+                else {
+                    arcadeMod_button.setVisibility(View.INVISIBLE);
+                    freeMod_button.setVisibility(View.INVISIBLE);
+                    isOpen=true;
+
+                }
+
             }
         });
     }
 }
+
 
 
 
