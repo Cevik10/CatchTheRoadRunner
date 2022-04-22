00fda31fea0d13c95fcd9fc94ad0a2c0c3e14b6b
ipobeatz <ipobeats7@gmail.com>
2022-04-22 17:33:33 +0300

    Start button'unun açılır ekranları yapıldı.

--- app/src/main/java/com/ibrahimcakir/catchtheroadrunner/MainActivity.java
+++ app/src/main/java/com/ibrahimcakir/catchtheroadrunner/MainActivity.java
@@ -1,16 +1,44 @@
 package com.ibrahimcakir.catchtheroadrunner;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.provider.FontRequest;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    Button button1;
+    Button button4;
+    Button button5;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        // Locate the buttons in your xml
+        button1 = (Button) findViewById(R.id.button1);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+
+        //set listener on button1
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //make button2 visible when button1 is pressed
+                button4.setVisibility(View.VISIBLE);
+                button5.setVisibility(View.VISIBLE);
+            }
+        });
+    }
 }
+
+
+
+
+
+
+
