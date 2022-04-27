fe47387bb38e9d4f2da0345f6608dcdf9066a62e
Hakan Çevik <hakanxcevik7@gmail.com>
2022-04-27 15:09:51 +0300

    vol11 pop-up window icons activated.

--- app/src/main/java/com/ibrahimcakir/catchtheroadrunner/GameScreen.java
+++ app/src/main/java/com/ibrahimcakir/catchtheroadrunner/GameScreen.java
@@ -2,31 +2,39 @@ package com.ibrahimcakir.catchtheroadrunner;
 
 import android.annotation.SuppressLint;
 import android.app.Dialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class GameScreen extends AppCompatActivity {
-    public int counter;
+
     ProgressBar mProgressBar;
     TextView timeText;
     CountDownTimer mCountDownTimer;
     Dialog myDialog;
-    Button showPopup_button;
+    ImageButton showPopup_button;
+
+
+    boolean isOn;
 
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
+
+        isOn = true;
 
         showPopup_button = findViewById(R.id.showPopup_button);
         myDialog = new Dialog(this);
@@ -59,21 +67,79 @@ public class GameScreen extends AppCompatActivity {
     }
 
 
+
     public void showPopup(View view) {
+
         TextView textClose;
+        ImageButton musicButton;
+        ImageButton backButton;
 
         myDialog.setContentView(R.layout.popupwindow);
 
         textClose = myDialog.findViewById(R.id.textClose);
+        musicButton = myDialog.findViewById(R.id.musicButton);
+        backButton = myDialog.findViewById(R.id.backButton);
+
+
+
         textClose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 myDialog.dismiss();
             }
         });
+
+
+
+        musicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isOn) {
+
+                    isOn = false;
+                    musicButton.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.music_icon));
+
+                }
+
+                else{
+
+                    isOn = true;
+                    musicButton.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.musicoff_icon));
+                }
+
+
+
+            }
+        });
+
+
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GameScreen.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
         myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         myDialog.show();
+
     }
 
 
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/ibrahimcakir/catchtheroadrunner/MainActivity.java
+++ app/src/main/java/com/ibrahimcakir/catchtheroadrunner/MainActivity.java
@@ -1,14 +1,10 @@
 package com.ibrahimcakir.catchtheroadrunner;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,16 +14,10 @@ public class MainActivity extends AppCompatActivity {
     boolean isOpen;
 
 
-
-
-
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
         isOpen = true;
 
